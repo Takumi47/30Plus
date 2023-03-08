f9b461a58669f01cb13a53776421c1a42feb63de
xanderchchiang <XanderCHChiang@mobiledrivetech.com>
2023-03-09 00:02:10 +0800

    HW - May 11, update 957

--- 30Plus/Problems/957_PrisonCellsAfterNDays.swift
+++ 30Plus/Problems/957_PrisonCellsAfterNDays.swift
@@ -12,7 +12,7 @@ class Solution957 {
         let t1 = (cells: [0,1,0,1,1,0,0,1], n: 7, output: [0,0,1,1,0,0,0,0])
         let t2 = (cells: [1,0,0,1,0,0,1,0], n: 1000000000, output: [0,0,1,1,1,1,1,0])
         [t1, t2].forEach { t in
-            let result = Xoo957().prisonAfterNDays(t.0, t.1)
+            let result = Xoo957_A().prisonAfterNDays(t.0, t.1)
             print("\(result)")
             if result != t.2 { print("❌❌❌❌❌\n") }
         }
@@ -38,5 +38,26 @@ class Xoo957 {
             }
         }
         return (0..<8).map { (k >> $0) & 1 }
+    }
+}
+
+// MARK: - Find the Loop Pattern
+
+/*
+ Proof:
+ https://math.stackexchange.com/questions/3311568/why-does-this-pattern-repeat-after-14-cycles-instead-of-256-can-you-give-a-proo/3311963#3311963
+ */
+
+class Xoo957_A {
+    func prisonAfterNDays(_ cells: [Int], _ n: Int) -> [Int] {
+        var cells = cells
+        for _ in 0..<1 + (n - 1) % 14 {
+            var curr = Array(repeating: 0, count: 8)
+            for i in 1..<7 {
+                curr[i] = (cells[i - 1] + cells[i + 1] + 1) % 2
+            }
+            cells = curr
+        }
+        return cells
     }
 }
